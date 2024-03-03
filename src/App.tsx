9eb4bc833eede0da0b3a18f8340d92e0d009589c
ishan-joshi-24 <f20170458@pilani.bits-pilani.ac.in>
2024-03-03 16:48:44 +0530

    feature(hangman): initial commit

---
+++ src/App.tsx
@@ -0,0 +1,111 @@
+import { useCallback, useEffect, useState } from 'react'
+import words from './wordList.json'
+import { HangmanDrawing } from './HangmanDrawing'
+import { HangmanWord } from './HangmanWord'
+import { Keyboard } from './Keyboard'
+
+function getWord() {
+    return words[Math.floor(Math.random() * words.length)]
+}
+
+function App() {
+    const [wordToGuess, setWordToGuess] = useState(getWord)
+
+    const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+
+    const incorrectLetters = guessedLetters.filter(
+        (letter) => !wordToGuess.includes(letter)
+    )
+
+    const isLoser = incorrectLetters.length >= 6
+    const isWinner = wordToGuess
+        .split('')
+        .every((letter) => guessedLetters.includes(letter))
+
+    const addGuessedLetter = useCallback(
+        (letter: string) => {
+            if (guessedLetters.includes(letter) || isWinner || isLoser) return
+            setGuessedLetters((currentLetters) => [...currentLetters, letter])
+        },
+        [guessedLetters, isWinner, isLoser]
+    )
+
+    useEffect(() => {
+        const handler = (e: KeyboardEvent) => {
+            const key = e.key
+
+            if (!key.match(/^[a-z]$/)) return
+
+            e.preventDefault()
+            addGuessedLetter(key)
+        }
+
+        document.addEventListener('keypress', handler)
+
+        return () => {
+            document.removeEventListener('keypress', handler)
+        }
+    }, [guessedLetters])
+
+    useEffect(() => {
+        const handler = (e: KeyboardEvent) => {
+            const key = e.key
+            if (key !== 'Enter') return
+            e.preventDefault()
+            setWordToGuess(getWord())
+            setGuessedLetters([])
+        }
+
+        document.addEventListener('keypress', handler)
+
+        return () => {
+            document.removeEventListener('keypress', handler)
+        }
+    }, [])
+
+    return (
+        <>
+            <h1
+                style={{
+                    textAlign: 'center',
+                }}
+            >
+                Lets play Hangman!
+            </h1>
+            <div
+                style={{
+                    maxWidth: '800px',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    gap: '2rem',
+                    margin: '0 auto',
+                    alignItems: 'center',
+                }}
+            >
+                <div style={{ fontSize: '2rem', textAlign: 'center' }}>
+                    {isWinner && 'Winner! - Refresh to try again!'}
+                    {isLoser &&
+                        `Nice Try! - The word was ${wordToGuess.toUpperCase()} Refresh to try again!`}
+                </div>
+                <HangmanDrawing numberOfGuesses={incorrectLetters.length} />
+                <HangmanWord
+                    guessedLetters={guessedLetters}
+                    wordToGuess={wordToGuess}
+                    reveal={isLoser}
+                />
+                <div style={{ alignSelf: 'stretch' }}>
+                    <Keyboard
+                        activeLetters={guessedLetters.filter((letter) =>
+                            wordToGuess.includes(letter)
+                        )}
+                        inactiveLetters={incorrectLetters}
+                        addGuessedLetter={addGuessedLetter}
+                        disabled={isWinner || isLoser}
+                    />
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default App
